d33e68d31c346b550fee612a0e702da936b6ede9
Dikshitha0812 <71216869+Dikshitha0812@users.noreply.github.com>
2021-09-07 09:01:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+  
+import pandas as pd
+import csv
+
+df = pd.read_csv("total_stars.csv")
+
+print(df.shape)
+del df ["Luminosity"]
+del df ["Star_name.1"]
+del df ["Distance.1"]
+del df ["Mass.1"]
+del df ["Radius.1"]
+del df ["Unnamed: 0"]
+del df ["Unnamed: 6"]
+
+df.to_csv("final.csv")
